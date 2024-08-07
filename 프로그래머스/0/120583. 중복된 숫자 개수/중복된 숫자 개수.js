d890c85be4ce1d7f82cdede6f4c0a3ef184ba38f
찌니 <wkddkcl02@naver.com>
2024-08-07 16:12:09 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,11 @@
+function solution(array, n) {
+    let count=0
+    for(let i=0; i<array.length; i++){
+        if(array[i]==n){
+            count++
+        }else{
+            continue
+        }
+    }
+    return count
+}
