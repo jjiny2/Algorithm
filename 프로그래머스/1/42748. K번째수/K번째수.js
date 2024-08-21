7fc602c8110dba095c33f0fd754c08e172433142
찌니 <wkddkcl02@naver.com>
2024-08-21 22:11:00 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,15 +1,14 @@
 function solution(array, commands) {
     let answer = [];
     for(let i=0; i<commands.length; i++){
+        let start=commands[i][0];
+        let j=commands[i][1];
+        let k=commands[i][2];
         
-        let start=commands[i][0]
-        let end=commands[i][1]
-        let num=commands[i][2]
-        
-        let newArray=array.slice(start-1,end);
-        
-        newArray.sort((a,b)=>a-b);
-        answer.push(newArray[num-1])
+        let newArray=array.slice(start-1,j).sort((a,b)=>(a-b));
+        answer.push(newArray[k-1])
     }
-    return answer
+    
+    
+    return answer;
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 32.9 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 29일 15:34:53
+2024년 08월 21일 22:11:03
 
 ### 문제 설명
 
