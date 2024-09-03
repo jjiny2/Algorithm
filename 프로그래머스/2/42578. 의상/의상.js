d235ecee0705c02e2e0a42afe42bc805cbd384f1
찌니 <wkddkcl02@naver.com>
2024-09-03 22:24:48 +0900

    [level 2] Title: 의상, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,15 +1,9 @@
 function solution(clothes) {
-    const clothesMap = new Map();
-    clothes.forEach(([item, type])=>{
-        if(clothesMap.has(type)){
-            clothesMap.set(type, clothesMap.get(type)+1)
-        }else{
-            clothesMap.set(type,1)
-        }
-    })
-    let count =1;
-    for(let value of clothesMap.values()){
-        count *=value+1
-    }
-    return count-1
+
+    
+const closeMap=clothes.reduce((map, [item, type])=>{
+    map.set(type, (map.get(type)||0)+1)
+    return map
+}, new Map());
+return [...closeMap.values()].reduce((count, value)=>count *=value+1, 1)-1
 }
