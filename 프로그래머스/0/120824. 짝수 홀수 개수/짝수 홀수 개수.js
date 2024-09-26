193ff2ed888cb9c0f381bd9b4aaedfef8438d7fc
찌니 <wkddkcl02@naver.com>
2024-09-26 14:51:48 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let answer = [];
+    let count1 =0;
+    let count2 =0;
+    for(let i=0; i<num_list.length; i++){
+        if(num_list[i]%2 == 0){
+            count1 ++
+        }else{
+            count2 ++
+        }
+    }
+    answer.push(count1, count2)
+    return answer;
+}
