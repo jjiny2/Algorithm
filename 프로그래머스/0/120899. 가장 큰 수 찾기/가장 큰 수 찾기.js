30f1d8068a706fd717ab59d651a06a28bea85059
찌니 <wkddkcl02@naver.com>
2024-10-14 11:16:11 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,6 +1,9 @@
 function solution(array) {
-    let max=Math.max(...array)
+    let max=0;
+    for(let i=0; i<array.length; i++){
+        if(array[i]>max){
+            max=array[i];
+        }
+    }
     return [max, array.indexOf(max)]
-    
-    
 }
