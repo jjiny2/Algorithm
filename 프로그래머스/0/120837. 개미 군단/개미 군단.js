f439b70ad5cf1d2543cebda882ee6ae77097e464
찌니 <wkddkcl02@naver.com>
2024-10-14 14:43:49 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,9 +1,12 @@
 function solution(hp) {
-    let a=Math.floor(hp/5)
-    hp=hp%5
-    let b=Math.floor(hp/3)
-    hp=hp%3
-    let c=Math.floor(hp/1)
-    hp=hp%1
-    return a+b+c
+    let count= Math.floor(hp/5) 
+    let last= hp%5 
+    
+    count+=Math.floor(last/3) 
+    last = last%3 
+    
+    count +=Math.floor(last/1)
+    last = last%1
+    
+    return count
 }
