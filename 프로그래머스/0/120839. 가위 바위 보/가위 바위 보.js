bcb68527d9bd393d51ee6eec3482cc7c67be6232
찌니 <wkddkcl02@naver.com>
2024-10-14 11:21:40 +0900

    [level 0] Title: 가위 바위 보, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,13 +1,13 @@
 function solution(rsp) {
-    let answer=''
+    let answer = '';
     for(let i=0; i<rsp.length; i++){
-        if(rsp[i]==2){
-            answer +=0
-        }else if(rsp[i] ==0){
-            answer +=5
+        if(rsp[i] == 2){
+            answer+='0'
+        }else if(rsp[i] == 0){
+            answer+='5'
         }else{
-            answer +=2
+            answer +='2'
         }
     }
-    return answer
+    return answer;
 }
