950f8b3510b65b35fe741c71273fc6f9ac3b6cdf
찌니 <wkddkcl02@naver.com>
2024-12-04 21:46:13 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,6 +1,4 @@
 function solution(n, m) {
-    const gcd = (n, m) => (m === 0 ? n : gcd(m, n % m));
-    const lcm = (n * m) / gcd(n, m);
-
-    return [gcd(n, m), lcm];
+    var answer = [];
+    return answer;
 }
